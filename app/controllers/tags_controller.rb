a55308631480e6343800eb663bfee567f824f03b
yalin <ywang28@ncsu.edu>
2014-03-30 10:13:10 -0700

    tag redirect to same photo

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -13,7 +13,7 @@ class TagsController < ApplicationController
     else
       photo.tag.create(photo_id: photo.id, xloc: xloc.to_i, yloc: yloc.to_i, width: width.to_i, height: height.to_i, user_name: sel_user.first_name + " " + sel_user.last_name)
     end
-    redirect_to user_photos_path(user)
+    redirect_to user_photos_path(user) + "#" + photo.file_name
 
   end
 
